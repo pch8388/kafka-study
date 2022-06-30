846c19d059e05fd470157fb2e575cc668c68ea53
sc <pch838811@gmail.com>
2022-06-30 20:04:15 +0900

    kafka stream join

---
+++ kafka-streams-start/src/main/java/me/study/KStreamJoinKTable.java
@@ -0,0 +1,40 @@
+package me.study;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+
+import java.util.Properties;
+
+@Slf4j
+public class KStreamJoinKTable {
+
+    private static final String APPLICATION_NAME = "order-join-application";
+    private static final String BOOTSTRAP_SERVERS = "my-kafka:9092";
+    private static final String ADDRESS_TABLE = "address";
+    private static final String ORDER_STREAM = "order";
+    private static final String ORDER_JOIN_STREAM = "order_join";
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_NAME);
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KTable<Object, Object> addressTable = builder.table(ADDRESS_TABLE);
+        KStream<Object, Object> orderStream = builder.stream(ORDER_STREAM);
+
+
+        orderStream.join(addressTable, (order, address) -> order + " send to " + address)
+                   .to(ORDER_JOIN_STREAM);
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+    }
+}
