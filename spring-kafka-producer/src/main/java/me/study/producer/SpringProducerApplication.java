4db6d21fd54778e44fd37e9f64e1271bc71f8f9e
sc <pch838811@gmail.com>
2022-07-27 21:47:52 +0900

    spring custom kafka template

--- spring-kafka-producer/src/main/java/me/study/producer/SpringProducerApplication.java
+++ spring-kafka-producer/src/main/java/me/study/producer/SpringProducerApplication.java
@@ -1,18 +1,25 @@
 package me.study.producer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
+@Slf4j
 @SpringBootApplication
 public class SpringProducerApplication implements CommandLineRunner {
 
     private static final String TOPIC_NAME = "test";
 
-    private final KafkaTemplate<Integer, String> template;
+    private final KafkaTemplate<String, String> template;
 
-    public SpringProducerApplication(KafkaTemplate<Integer, String> template) {
+    public SpringProducerApplication(@Qualifier("customKafkaTemplate") KafkaTemplate<String, String> template) {
         this.template = template;
     }
 
@@ -22,10 +29,26 @@ public class SpringProducerApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
-        for (int i = 0; i < 10; i++) {
-            template.send(TOPIC_NAME, "test" + i);
-        }
-        System.exit(0);
+    public void run(String... args) throws InterruptedException {
+//        for (int i = 0; i < 10; i++) {
+//            template.send(TOPIC_NAME, "test" + i);
+//        }
+//        System.exit(0);
+
+        ListenableFuture<SendResult<String, String>> future = template.send(TOPIC_NAME, "test-custom-01");
+        future.addCallback(new KafkaSendCallback<>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("success");
+            }
+
+            @Override
+            public void onFailure(KafkaProducerException ex) {
+                log.info("fail");
+            }
+        });
+
+        Thread.sleep(1000);
     }
 }
